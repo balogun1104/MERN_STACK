0a8de880e0faa71bb5e5564502ba95c14af09508
Balogun Razak <razak2free@gmail.com>
2022-08-16 11:25:36 +0100

    16/08/22

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,29 @@
+import { useEffect, useState } from "react"
+import WorkoutDetails from "../components/WorkoutDetails"
+
 const Home = () => {
+    const [workouts, setWorkouts] = useState(null)
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+           const response =  await fetch('/api/workouts')
+           const json = await response.json()
+       
+           if(response.ok) {
+               setWorkouts(json)
+           }
+        }
+        fetchWorkouts()
+       }, [])
+
     return (
-        <div>This is the HomePage</div>
+      <div className="home">
+        <div className="workouts">
+             {workouts && workouts.map((workout) => (
+              <WorkoutDetails key={workout._id} workout={workout} />
+             ))}
+        </div>
+      </div>
     )
 }
 
