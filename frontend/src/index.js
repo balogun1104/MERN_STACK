79694ae9a4337160f57a7ae1b077ca54cdd38155
Balogun Razak <razak2free@gmail.com>
2022-08-23 23:46:42 +0100

    testing+

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { WorkoutsContextProvider } from './context/WorkoutContext';
+import { WorkoutsContextProvider } from './context/WorkoutsContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,32 +1,36 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
+
+// components
 import WorkoutDetails from "../components/WorkoutDetails"
 import WorkoutForm from "../components/WorkoutForm"
 
 const Home = () => {
-    const [workouts, setWorkouts] = useState(null)
+  const { workouts, dispatch } = useWorkoutsContext()
 
-    useEffect(() => {
-        const fetchWorkouts = async () => {
-           const response =  await fetch('/api/workouts')
-           const json = await response.json()
-       
-           if(response.ok) {
-               setWorkouts(json)
-           }
-        }
-        fetchWorkouts()
-       }, [])
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch('/api/workouts')
+      const json = await response.json()
 
-    return (
-      <div className="home">
-        <div className="workouts">
-             {workouts && workouts.map((workout) => (
-              <WorkoutDetails key={workout._id} workout={workout} />
-             ))}
-        </div>
-        <WorkoutForm />
+      if (response.ok) {
+        dispatch({type: 'SET_WORKOUTS', payload: json})
+      }
+    }
+
+    fetchWorkouts()
+  }, [dispatch])
+
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts && workouts.map(workout => (
+          <WorkoutDetails workout={workout} key={workout._id} />
+        ))}
       </div>
-    )
+      <WorkoutForm />
+    </div>
+  )
 }
 
-export default Home
+export default Home 
