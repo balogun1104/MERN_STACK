103da28acbfa66dece25308d4901aa55a6111da1
Balogun Razak <razak2free@gmail.com>
2022-08-25 23:52:33 +0100

    25/08/22

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
         <header>
              <div className="container">
             <Link to='/'>
-                <h1>Workout Buddy</h1>
+                <h1>Workout Manager</h1>
             </Link>
         </div>
         </header>
