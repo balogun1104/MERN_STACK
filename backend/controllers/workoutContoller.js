4d2f3cabe4e22e16be68579a7665ef5ccb0109b7
Balogun Razak <razak2free@gmail.com>
2022-08-25 23:46:49 +0100

    25/08/22

--- backend/controllers/workoutContoller.js
+++ backend/controllers/workoutContoller.js
@@ -30,6 +30,22 @@ const getWorkout = async (req, res) => {
 const createWorkout = async(req, res) => {
     const {title, load, reps} = req.body
 
+
+    let emptyFields = []
+
+    if (!title) {
+      emptyFields.push('title')
+    }
+    if (!load) {
+      emptyFields.push('load')
+    }
+    if (!reps) {
+      emptyFields.push('reps')
+    }
+    if (emptyFields.length > 0) {
+      return res.status(400).json({ error: 'Please fill in all fields', emptyFields })
+    }
+
     // adding doc to db
     try {
         const workout = await Workout.create({title, load, reps})
--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,72 +1,75 @@
-import { useState } from "react";
+import { useState } from 'react'
 import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
 
 const WorkoutForm = () => {
-    const { dispatch } = useWorkoutsContext()
+  const { dispatch } = useWorkoutsContext()
 
-     const [title, setTitle] = useState('');
-     const [reps, setReps] = useState('');
-     const [load, setLoad] = useState('')
-     const [error, setError] = useState(null)
+  const [title, setTitle] = useState('')
+  const [load, setLoad] = useState('')
+  const [reps, setReps] = useState('')
+  const [error, setError] = useState(null)
+  const [emptyFields, setEmptyFields] = useState([])
 
-//SUbmit Handler
-const handleSubmit = async (event) => {
-    event.preventDefault()
+  const handleSubmit = async (e) => {
+    e.preventDefault()
 
-    const workout = {title, reps, load}
-
+    const workout = {title, load, reps}
+    
     const response = await fetch('/api/workouts', {
-        method: 'POST',
-        body: JSON.stringify(workout),
-        headers: {
-            'Content-Type': 'application/json'
-        }
+      method: 'POST',
+      body: JSON.stringify(workout),
+      headers: {
+        'Content-Type': 'application/json'
+      }
     })
     const json = await response.json()
 
-    //checking for error
-    if(!response.ok) {
-        setError(json.error)
+    if (!response.ok) {
+      setError(json.error)
+      setEmptyFields(json.emptyFields)
     }
-    if(response.ok) {
-        setTitle('')
-        setLoad('')
-        setReps('')
-        setError(null)
-        console.log('new workout found ', json)
-
-        dispatch({type: 'CREATE_WORKOUT', payload: json})
+    if (response.ok) {
+      setEmptyFields([])
+      setError(null)
+      setTitle('')
+      setLoad('')
+      setReps('')
+      dispatch({type: 'CREATE_WORKOUT', payload: json})
     }
-} 
 
-     return (
- <form className="create" onSubmit={handleSubmit}>
-    <h3>Add a New Workout</h3>
+  }
 
-    <label>Excersize Title</label>
-    <input 
-    type='text'
-    onChange={(e) => setTitle (e.target.value)}
-    value={title}
-    />
+  return (
+    <form className="create" onSubmit={handleSubmit}> 
+      <h3>Add a New Workout</h3>
 
-<label>Load (in Kg) :</label>
-    <input 
-    type='number'
-    onChange={(e) => setLoad (e.target.value)}
-    value={load}
-    />
+      <label>Excersize Title:</label>
+      <input 
+        type="text" 
+        onChange={(e) => setTitle(e.target.value)} 
+        value={title}
+        className={emptyFields.includes('title') ? 'error' : ''}
+      />
 
+      <label>Load (in kg):</label>
+      <input 
+        type="number" 
+        onChange={(e) => setLoad(e.target.value)} 
+        value={load}
+        className={emptyFields.includes('load') ? 'error' : ''}
+      />
 
-<label>Reps: </label>
-    <input 
-    type='number'
-    onChange={(e) => setReps (e.target.value)}
-    value={reps}
-    />
-    <button> Add Workout </button>  
-    {error && <div className="error"> {error} </div>}  
- </form>
+      <label>Number of Reps:</label>
+      <input 
+        type="number" 
+        onChange={(e) => setReps(e.target.value)} 
+        value={reps}
+        className={emptyFields.includes('reps') ? 'error' : ''}
+      />
+
+      <button>Add Workout</button>
+      {error && <div className="error">{error}</div>}
+    </form>
   )
 }
 
